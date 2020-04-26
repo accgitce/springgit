613dca419cfb7297e233fe2f022d513e9551de3f
zhangxinchao <ceshizhang@.com>
2020-04-26 13:20:24 +0800

    第三次提交

--- src/main/java/com/atxinyuan/springgit/controller/HelloController.java
+++ src/main/java/com/atxinyuan/springgit/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.atxinyuan.springgit.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * describe:
  *
  * @author zhangxinchao
  * @date 2020/04/26
  */
+@RestController
 public class HelloController {
 }
